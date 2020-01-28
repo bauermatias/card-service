b63dc68674b6cc911f3f17550d35c9b6cad17c6e
kyronte <sid3windeR>
2020-01-28 11:49:38 -0300

    Adding assertions to test

--- src/test/java/sideboard/wizard/cardservice/repositories/CardRepositoryTests.java
+++ src/test/java/sideboard/wizard/cardservice/repositories/CardRepositoryTests.java
@@ -1,9 +1,12 @@
 package sideboard.wizard.cardservice.repositories;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import sideboard.wizard.cardservice.entities.Card;
+
+import java.util.Optional;
 
 @SpringBootTest
 public class CardRepositoryTests {
@@ -17,9 +20,11 @@ public class CardRepositoryTests {
         card.setName("Ajani's Pridemate");
 
         Card savedCard = cardRepository.save(card);
-        //assertion savedCard.getId() != null
 
-        cardRepository.findById(savedCard.getId());
+        Optional<Card> retrievedCard = cardRepository.findById(savedCard.getId());
+        if(retrievedCard.isPresent()) {
+            Assertions.assertEquals(card.getName(), retrievedCard.get().getName());
+        }
 
     }
 }
